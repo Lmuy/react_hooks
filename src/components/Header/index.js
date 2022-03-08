3cdecd3e86054e533cf36eebd499c428892c493f
liukangning <liukangning@kingsoft.com>
2022-03-08 10:26:26 +0800

    hooks组件间传值

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,26 +2,24 @@ import React from 'react';
 import { Button } from 'antd';
 import './header.scss'
 
-const login = () => {
-  return (
+function Header(props) {
+  const { background, changeColor } = props
+  const login = (
     <div>
       <Button type="text">退出</Button>
+      <Button type="text" onClick={changeColor}>换肤</Button>
     </div>
   )
-}
-const unLogin = () => {
-  return (
+  const unLogin = (
     <div>
       <Button type="text">注册</Button>
       <Button type="text">登陆</Button>
     </div>
   )
-}
 
-function Header() {
   const editButton = localStorage.getItem('username') ? login : unLogin
   return (
-    <div className="header">
+    <div className="header" style={{background: background}}>
       <span>元气鉴赏平台</span>
       { editButton }
     </div>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom'
 import Header from '../../components/Header'
 import { Button } from 'antd';
@@ -15,13 +15,20 @@ function Home(props) {
     navigate('/layout')
   }
 
+  const [color, setColor] = useState('transparent')
   useEffect(() => {
     console.log(params)
+    handleColor()
   },[])
+  const handleColor = () => {
+    const arr = ['red', 'black', 'yellow', 'transparent']
+    const No = parseInt(Math.random()*3)
+    setColor(arr[No])
+  }
 
   return (
     <div className="home">
-      <Header />
+      <Header background={color} changeColor={handleColor}/>
       <h1>Home Page</h1>
       <div>接收到的参数是
         {Object.keys(params.state).map((key) => (
