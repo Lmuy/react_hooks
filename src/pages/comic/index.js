ed62e844e9c86edc1c9af0f9f686e3598bfa7a51
liukangning <liukangning@kingsoft.com>
2022-03-09 18:03:58 +0800

    0309更新

--- src/pages/comic/index.js
+++ src/pages/comic/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 function Comic() {
+  const timer = useRef()
+  let i = 0
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    timer.current = setInterval(() => {
+      if (count < 10) {
+        setCount(i++)
+      } else {
+        clearInterval(timer.current)
+      }
+    }, 1000)
+    return () => {
+      clearInterval(timer.current)
+    }
+  }, [])
+
   return (
-    <div>comic</div>
+    <>
+      <div>comic</div>
+      <div>{count}</div>
+    </>
   );
 }
 
